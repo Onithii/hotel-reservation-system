b5b01bcca7ef8fe713512beefaa2c320402317c6
Onithii <wkonithi11@gmail.com>
2026-06-24 11:44:01 +0530

    feat(repository): add ReservationRepository interface

--- src/main/java/com/example/hotel_reservation_system/repository/ReservationRepository.java
+++ src/main/java/com/example/hotel_reservation_system/repository/ReservationRepository.java
@@ -2,11 +2,11 @@ package com.example.hotel_reservation_system.repository;
 
 import com.example.hotel_reservation_system.model.Reservation;
 import org.springframework.data.jpa.repository.JpaRepository;
-
+import org.springframework.stereotype.Repository;
 import java.util.List;
 
+@Repository
 public interface ReservationRepository extends JpaRepository<Reservation, Long> {
-
-    // ROOM CHECK (UPDATED - NO STATUS)
-    boolean existsByRoomNumber(int roomNumber);
+    // Custom query method to find all reservations belonging to a specific User ID
+    List<Reservation> findByUserId(Long userId);
 }
